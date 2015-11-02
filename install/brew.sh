1250e7eaa7639f99b8c0299c6483c1b1a6d3f1b0
Marcus Bengtsson <marcus.bengtsson@hiq.se>
2015-11-02 08:17:25 +0100

    Added jenv

--- install/brew.sh
+++ install/brew.sh
@@ -2,9 +2,11 @@
 
 brew install android-sdk
 brew install bash-completion
+brew install caskroom/cask/brew-cask
 brew install git
 brew install gradle
 brew install htop-osx
+brew install isl
 brew install kdiff3
 brew install libevent
 brew install libxml2
@@ -22,6 +24,10 @@ brew install unrar
 brew install wget
 brew install youtube-dl
 brew install zsh
+
+brew cask install java
+brew cask install java7
+brew cask install qt-creator
 
 brew linkapps macvim
 brew linkapps kdiff3
